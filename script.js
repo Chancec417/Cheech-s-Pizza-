c30f3354705f2f546b0fa415c5dd92a281fc0472
CLEVENHAGEN <cc0884895@otc.edu>
2019-02-27 16:50:38 -0600

    (another button)

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 
 
-document.getElementById(`but1`).addEventListener("click", addItems);
 
-var pizzaType = ["Cheese", "Pepperoni", "Veggie", "Meat Lovers", "Hawaiin" ];s
+var pizzas = ["Cheese", "Pepperoni", "Veggie", "Meat Lovers", "Hawaiin" ];
+
+document.getElementById(`but1`).addEventListener("click", addItems);
 
 function addItems() {
     taskList = document.getElementById(`list`);
-    for(var i = 0; i < pizzaType.length; i++) {
-        taskList.innerHTML += "<li>" + pizzaTpe[i] + "</li>";
-    }
+    for(var i = 0; i < pizzas.length; i++) {
+        taskList.innerHTML += "<li>" + pizzas[i] + "</li>";
+    } 
 }
 
+
+//Need to drop have a drop down menu for the pizza, also it says
+//that they can only order one type of pizza, which will make things easier. 
+
+// They will pick the pizza choice
+
+//Then how many they want    
+
+//Could write out the list of Pizza before the choice, 
+//then type in choice of pizza, then how many? 
+
+document.getElementById(`but2`).addEventListener("click", orderTotal);
+
+//Order Button, will call this method, will print out final order list
+function orderTotal() {
+    tasklist = document.getElementById(`orderList`);
+    
+        taskList.innerHTML += "<li>" +;
+
+}
