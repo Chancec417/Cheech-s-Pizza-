9e622760e6e2e5486d3670b46b51d87bf1cef99e
CLEVENHAGEN <cc0884895@otc.edu>
2019-02-27 15:47:41 -0600

    (Added Button)

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+document.getElementById(`but1`).addEventListener("click", addItems);
+
+var pizzaType = ["Cheese", "Pepperoni", "Veggie", "Meat Lovers", "Hawaiin" ];s
+
+function addItems() {
+    taskList = document.getElementById(`list`);
+    for(var i = 0; i < pizzaType.length; i++) {
+        taskList.innerHTML += "<li>" + pizzaTpe[i] + "</li>";
+    }
+}
