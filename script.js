a850ff3c2e16ac68bb5f19310383d24549a492ff
DAVID LECLERC <dl0586657@otc.edu>
2019-02-27 20:32:05 -0600

    added pizza selector

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 
 
 
-var pizzas = ["Cheese", "Pepperoni", "Veggie", "Meat Lovers", "Hawaiin" ];
+var pizzas = ["Cheese", "Pepperoni", "Veggie", "Meat Lovers", "Hawaiian" ];
 
 document.getElementById(`but1`).addEventListener("click", addItems);
 
-function addItems() {
-    taskList = document.getElementById(`list`);
+
+//displays pizza types, we might be able to set option.value to a second array 
+//with prices for each type of pizzas, I haven't played with it yet
+window.onload = addPizzas();
+function addPizzas(){
+    pizzaTypes = document.getElementById('typeSelect');
     for(var i = 0; i < pizzas.length; i++) {
-        taskList.innerHTML += "<li>" + pizzas[i] + "</li>";
+        var option = document.createElement('option');
+        option.innerHTML = pizzas[i];
+        option.value = pizzas[i]; //might be able to store prices here?
+        pizzaTypes.appendChild(option);
+              
     } 
 }
+
+
+function addItems() {
+    taskList = document.getElementById(`list`);
+    
+}
+
 
 
 //Need to drop have a drop down menu for the pizza, also it says
@@ -29,6 +44,6 @@ document.getElementById(`but2`).addEventListener("click", orderTotal);
 function orderTotal() {
     tasklist = document.getElementById(`orderList`);
     
-        taskList.innerHTML += "<li>" +;
+       taskList.innerHTML += "<li>" +;
 
 }
