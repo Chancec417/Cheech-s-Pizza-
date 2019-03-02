c39d3b4d495701c588f28ff6a8be4f561724127e
DAVID LECLERC <dl0586657@otc.edu>
2019-03-02 16:47:54 -0600

    added order total function
    added order total function

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 
 var pizzas = ["Cheese", "Pepperoni", "Veggie", "Meat Lovers", "Hawaiian" ];
+var prices = [7.99,8.99,9.99,10.99,9.99];
 
-//document.getElementById(`but1`).addEventListener("click", addItems);
 
 
 //displays pizza types, we might be able to set option.value to a second array 
@@ -14,17 +14,22 @@ function addPizzas(){
     for(var i = 0; i < pizzas.length; i++) {
         var option = document.createElement('option');
         option.innerHTML = pizzas[i];
-        option.value = pizzas[i]; //might be able to store prices here?
+        option.value = prices[i]; 
         pizzaTypes.appendChild(option);
               
     } 
 }
+button1 = document.getElementById(`but1`);
+button1.onclick = addItems;
+ function addItems(){
+    var pizzaSelection = document.getElementById(`typeSelect`);
+    var price = pizzaSelection.options[pizzaSelection.selectedIndex].value;
+    var pizzaQty = document.getElementById('pizzaQty');
+    var quantity = pizzaQty.options[pizzaQty.selectedIndex].value;
+    var total = quantity * price;
+    window.alert("your total is: $" + total +"\n if this is correct please click place order");
+    }
 
-
-function addItems() {
-    taskList = document.getElementById(`list`);
-    
-}
 
 
 
@@ -38,7 +43,7 @@ function addItems() {
 //Could write out the list of Pizza before the choice, 
 //then type in choice of pizza, then how many? 
 
-document.getElementById(`but2`).addEventListener("click", orderTotal);
+//document.getElementById(`but2`).addEventListener("click", orderTotal);
 
 //Order Button, will call this method, will print out final order list
 function orderTotal() {
